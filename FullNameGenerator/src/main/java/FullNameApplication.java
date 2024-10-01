65b26c8a24e5ca365be66ff626ca65449e05408c
sabuharb <abuharb.sandy@gmail.com>
2024-10-01 11:35:45 -0500

    1st commit

---
+++ FullNameGenerator/src/main/java/FullNameApplication.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+import java.lang.invoke.StringConcatException;
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter your first name: ");
+        boolean firstName = scanner.hasNextLine();
+
+        System.out.print("Please enter your middle name (press enter if none): ");
+        String middleName = scanner.nextLine().trim();
+
+        System.out.print("Please enter your last name: ");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.print("Please enter your suffix (press enter if none): ");
+        String suffix = scanner.nextLine().trim();
+
+        if (!suffix.isEmpty()) {
+            String fullName = ", " + suffix;
+
+            System.out.println("Full name: " + fullName);
+        }
+
+
+
+    }
+}
